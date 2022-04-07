8b76615be8a916a4086cfff3e6dad35b8ee84b73
Yugesh Palvai <yugeshpalvai21@gmail.com>
2022-04-07 16:57:28 +0530

    adds more validations for Property

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,3 +1,5 @@
 class Property < ApplicationRecord
   validates :address, presence: true
+  validates :size, presence: true
+  validates :price, presence: true
 end
--- spec/models/property_spec.rb
+++ spec/models/property_spec.rb
@@ -3,5 +3,7 @@ require 'rails_helper'
 RSpec.describe Property, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:address) }
+    it { should validate_presence_of(:size) }
+    it { should validate_presence_of(:price) }
   end
 end
