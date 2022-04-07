b6bb2aa0e4eeebcd8a9e9e017d28851a0d6d9e74
Yugesh Palvai <yugeshpalvai21@gmail.com>
2022-04-07 18:18:23 +0530

    Property create spec with invalid attributes

--- spec/requests/properties/create_spec.rb
+++ spec/requests/properties/create_spec.rb
@@ -14,4 +14,17 @@ describe 'POST /properties' do
       expect(response.status).to eq 201
     end 
   end
+  describe 'invalid property attributes' do
+    it 'returns error status' do
+      post '/properties', params: { 
+        property: {
+          address: "",
+          size: 1234,
+          price: 3452.23,
+          year_built: 2020
+        }
+      }
+      expect(response.status).to eq 422
+    end 
+  end
 end
