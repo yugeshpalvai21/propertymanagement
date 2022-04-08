10480bb16a79d9444ac9cab85afbea4f1aaffa2c
Yugesh Palvai <yugeshpalvai21@gmail.com>
2022-04-08 19:05:22 +0530

    updates request spec for Property#update action to check authentication

--- spec/requests/properties/update_spec.rb
+++ spec/requests/properties/update_spec.rb
@@ -1,9 +1,42 @@
 require 'rails_helper'
 
 describe 'PUT /properties' do
-  describe 'valid property attributes' do
-    let!(:property) { create(:property) }
-    it 'returns success status' do
+  let(:user) { create(:user) }
+  let!(:property) { create(:property) }
+  describe 'authenticated user' do
+    describe 'valid property attributes' do  
+      it 'returns success status' do
+        put "/properties/#{property.id}", params: { 
+          property: {
+            address: 'different address',
+            size: 1234,
+            price: 3452.23,
+            year_built: 2020
+          }
+        }, headers: { 'X-Username': user.username, 'X-Token': user.authentication_token }
+        expect(response).to have_http_status(:created)
+        expect(property.reload.address).to eq 'different address'
+      end 
+    end
+    
+    describe 'invalid property attributes' do
+      it 'returns error status' do
+        put "/properties/#{property.id}", params: { 
+          property: {
+            address: "",
+            size: 1234,
+            price: 3452.23,
+            year_built: 2020
+          }
+        }, headers: { 'X-Username': user.username, 'X-Token': user.authentication_token }
+        expect(response).to have_http_status(:unprocessable_entity)
+        json = JSON.parse(response.body).deep_symbolize_keys
+        expect(json[:address]).to eq(["can't be blank"])
+      end 
+    end
+  end
+  describe 'un-authenticated user' do
+    it 'returns invalid user response' do
       put "/properties/#{property.id}", params: { 
         property: {
           address: 'different address',
@@ -12,23 +45,10 @@ describe 'PUT /properties' do
           year_built: 2020
         }
       }
-      expect(response).to have_http_status(:created)
-      expect(property.reload.address).to eq 'different address'
-    end 
-  end
-  
-  describe 'invalid property attributes' do
-    let!(:property) { create(:property) }
-    it 'returns error status' do
-      put "/properties/#{property.id}", params: { 
-        property: {
-          address: "",
-          size: 1234,
-          price: 3452.23,
-          year_built: 2020
-        }
-      }
+
       expect(response).to have_http_status(:unprocessable_entity)
-    end 
+      json = JSON.parse(response.body).deep_symbolize_keys
+      expect(json[:message]).to eq "In Valid User"
+    end
   end
 end
