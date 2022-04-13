69208f4702938aef6734391a35d89fee25fd5a2b
Yugesh Palvai <yugeshpalvai21@gmail.com>
2022-04-13 18:57:30 +0530

    generates migration to add email and password_digest to Users with schema level constraints

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_08_115111) do
+ActiveRecord::Schema.define(version: 2022_04_13_121319) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,8 @@ ActiveRecord::Schema.define(version: 2022_04_08_115111) do
     t.string "authentication_token"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email", null: false
+    t.string "password_digest", null: false
   end
 
 end
