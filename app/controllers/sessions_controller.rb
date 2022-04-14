16225c8ce5731602656a2cc4786c8ddbaa81dd34
Yugesh Palvai <yugeshpalvai21@gmail.com>
2022-04-14 15:45:48 +0530

    implements functionality for generate json token if user authenticated

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,11 @@
 class SessionsController < ApplicationController
+  include JsonWebToken
+
   def create
     @user = User.find_by(email: params[:email])
 
     if @user && @user.authenticate(params[:password])
-      headers["X-Auth-Token"] = SecureRandom.base64(13)
+      headers["X-Auth-Token"] = jwt_encode(user_id: @user.id)
       render json: { message: "Logged In Successfully" }, status: :ok
     else
       render json: { message: "Something went wrong" }, status: :unprocessable_entity
